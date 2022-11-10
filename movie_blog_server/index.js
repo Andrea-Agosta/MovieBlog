02a29bd646185dcf40c67408bc1dd1c635102d96
Andrea-Agosta <andrea.agosta85@gmail.com>
2022-11-10 09:23:36 +0100

    add routing getData from api

--- movie_blog_server/index.js
+++ movie_blog_server/index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const { testDb } = require('./database/database');
-const { moviesAPI } = require('./moviesAPI');
+const { moviesAPI } = require('./api/moviesAPI');
 const app = express();
 
 app.use(express.static("public"));
+app.use('/api/movie', movie);
+
 
 // testDb(); CONNECTION MONGO
+
+
 
 app.get('/api', (req, res) => {
   // moviesAPI(); //CONNECTION API
